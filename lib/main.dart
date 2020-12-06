014b6b3ce098c355adda2057342fb6092f4cefb5
Ettore Toma <ettore.toma@gmail.com>
2020-12-06 21:10:47 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:dribbble_recipe_app/widgets/searchbar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cooking app',
+      theme:
+          ThemeData(primaryColor: Colors.white, backgroundColor: Colors.white),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'What to eat?',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.filter_alt_outlined),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          SearchBar(),
+        ],
+      ),
+    );
+  }
+}
